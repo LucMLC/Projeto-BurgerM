742391d0b2f00859e2720a314a6dbeffa99f0f9a
LucMLC <lucas.docanto08@gmail.com>
2024-01-07 18:19:06 -0300

    Atualização versão mobile

---
+++ menu/menu.js
@@ -0,0 +1,8 @@
+const menuOptions = [
+    { name: 'Super Vegano', price: 90, vegan: true, src: '../cardapio/burgerTwo.jpg' },
+    { name: 'Big shedder', price: 130, vegan: false, src: '../cardapio/burgerSeven.jpg' },
+    { name: 'Ligth Vegano', price: 90, vegan: true, src: '../cardapio/burgerOn.jpg' },
+    { name: 'Big Combo', price: 150, vegan: false, src: '../cardapio/burguerFor.jpg' },
+    { name: 'Turbinado', price: 130, vegan: false, src: '../cardapio/burgerFive.jpg' },
+    { name: 'Delicia Familia', price: 250, vegan: false, src: '../cardapio/BurgerSix.jpg' },
+    ]
