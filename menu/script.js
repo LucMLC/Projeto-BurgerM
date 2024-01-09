066fb9040ee0bed6253f0fe5a3c0379557921a6e
LucMLC <lucas.docanto08@gmail.com>
2024-01-09 01:16:49 -0300

    Add comentarios aos trechos do codigo

--- menu/script.js
+++ menu/script.js
@@ -1,10 +1,10 @@
 AOS.init();
 
 //Abrir - fechar - e finalizar compras
-const cart = document.querySelector('.cart');
-const fecharCart = document.querySelector('.add-itens');
-const cartOpen = document.querySelector('.cart-button');
-const totalDeDesconto = document.querySelector('.total-de-desconto');
+const cart = document.querySelector('.cart')
+const fecharCart = document.querySelector('.add-itens')
+const cartOpen = document.querySelector('.cart-button')
+const totalDeDesconto = document.querySelector('.total-de-desconto')
 
 //Abrir carrinho
 const cartVisibel = () => {
@@ -24,78 +24,78 @@ const completePurchases = () => {
     completePurchase.addEventListener('click', () => {
         // tira todos os elementos <li> do carrinho
         const itemsInCart = document.querySelectorAll('ul li');
-        itemsInCart.forEach(item => item.remove());
+        itemsInCart.forEach(item => item.remove())
 
         //Gera um alerta com agradecimento da compra
         alert('Sua compra foi realizada com sucesso. 😘 ❤️ Volte sempre!')
 
         // serve para se necessário redefinir o valor total para 0
-        valorTotal = 0;
+        valorTotal = 0
 
         // Atualiza o conteúdo do elemento HTML que exibe o valor total
-        totalDeDesconto.textContent = 'Seu valor total a pagar: R$0.00';
+        totalDeDesconto.textContent = 'Seu valor total a pagar: R$0.00'
 
-        //Chama a função para fechar o carrinho
+        //Chama a função de fechar o carrinho
         backCart()
     });
 };
 
 // Chama a função para completar as compras
-completePurchases();
+completePurchases()
 
 // Adicionar produtos
-const myUl = document.querySelector('ul');
-const comprar = document.querySelectorAll('.comprar');
+const myUl = document.querySelector('ul')
+const comprar = document.querySelectorAll('.comprar')
 
-let valorTotal = 0;
+let valorTotal = 0
 
 for (let i = 0; i < comprar.length; i++) {
     comprar[i].addEventListener('click', function (event) {
-        const clickedButton = event.target;
-        const productContainer = clickedButton.parentElement;
-        const productsPrice = parseFloat(productContainer.querySelector('.price-product').innerText.replace('R$', '').replace(',', '.').trim());
-        const productName = productContainer.querySelector('.name-product').innerText;
+        const clickedButton = event.target
+        const productContainer = clickedButton.parentElement
+        const productsPrice = parseFloat(productContainer.querySelector('.price-product').innerText.replace('R$', '').replace(',', '.').trim())
+        const productName = productContainer.querySelector('.name-product').innerText
 
-        // Cria um novo elemento li para o novo produto
+        // Cria uma li para o novo produto
         const newProductLi = document.createElement('li');
-        newProductLi.innerHTML = `${productName} - ${productsPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} <button class="remover">Remover</button>`;
+        newProductLi.innerHTML = `${productName} - ${productsPrice.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} <button class="remover">Remover</button>`
 
         // Adiciona o novo li à ul existente
-        myUl.appendChild(newProductLi);
+        myUl.appendChild(newProductLi)
 
-        alert('Produto adicionado ao carrinho');
+        alert('Produto adicionado ao carrinho')
 
-        calculateDesconto(productsPrice);
+        calculateDesconto(productsPrice)
 
-        // Adiciona evento de remoção ao novo botão de remoção
-        const newRemoveButton = newProductLi.querySelector('.remover');
+        // Adiciona evento de remoção novo botão de remoção
+        const newRemoveButton = newProductLi.querySelector('.remover')
         newRemoveButton.addEventListener('click', function () {
-            newProductLi.remove();
-            calculateDesconto(-productsPrice); // Remover o desconto quando um produto é removido
+            newProductLi.remove()
+            calculateDesconto(-productsPrice) // Remover o desconto quando o produto é removido
 
-        });
+        })
 
 
 
-    });
+    })
 }
 
 // Calcular desconto
 function calculateDesconto(valor) {
     if (valor > 100) {
-        valor *= 0.95;
+        valor *= 0.95
     }
 
-    valorTotal += valor;
+    valorTotal += valor
 
-    totalDeDesconto.textContent = `Valor total a pagar, já com desconto: R$${valorTotal.toFixed(2)}`;
+    totalDeDesconto.textContent = `Valor total a pagar, já com desconto: R$${valorTotal.toFixed(2)}`
 }
 
 
 // Ouvintes
 
-cartOpen.addEventListener('click', cartVisibel);
-fecharCart.addEventListener('click', backCart);
+cartOpen.addEventListener('click', cartVisibel)
+fecharCart.addEventListener('click', backCart)
 filtrar.addEventListener('click', filter)
 
 
